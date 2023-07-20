9622755b90cb0ac3d3b8720d2d1907cdc3543b2c
Sadock Iran <sadock@Sadocks-MacBook-Air.local>
2023-07-19 19:22:25 -0500

    Add hashing function for security

--- app.js
+++ app.js
@@ -4,7 +4,9 @@ const ejs = require("ejs");
 const express = require("express");
 const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
-const encrypt = require("mongoose-encryption");
+// const encrypt = require("mongoose-encryption");
+
+const md5 = require("md5")
 
 const app = express();
 
@@ -19,7 +21,7 @@ const userSchema = new mongoose.Schema({
     password: String
 });
 
-userSchema.plugin(encrypt, {secret: process.env.SECRET , encryptedFields : ["password"]});
+//userSchema.plugin(encrypt, {secret: process.env.SECRET , encryptedFields : ["password"]});
 
 const User = new mongoose.model("User", userSchema);
 
@@ -39,7 +41,7 @@ app.get("/login", function (req, res){
 app.post("/register", async (req, res) => {
     const newUser = new User({
         email : req.body.username,
-        password: req.body.password
+        password: md5(req.body.password)
     });
 
     newUser.save().then(()=>{
@@ -52,7 +54,7 @@ app.post("/register", async (req, res) => {
 
 app.post("/login", function (req, res){
     const username = req.body.username;
-    const password = req.body.password;
+    const password = md5(req.body.password);
 
     User.findOne({email: username})
     .then((foundUser)=>{
